34d796d82bc531dd404da632f9d75a2ad3bd70fa
modicristian <41091536+modicristian@users.noreply.github.com>
2018-08-21 12:44:33 -0300

    Comienzo armado de LAYOUT

--- app/src/main/java/com/example/mono/ejercicio_inicio_modulo_3/interfaz/MainActivity.java
+++ app/src/main/java/com/example/mono/ejercicio_inicio_modulo_3/interfaz/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.mono.ejercicio_inicio_modulo_3;
+package com.example.mono.ejercicio_inicio_modulo_3.interfaz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.mono.ejercicio_inicio_modulo_3.R;
 
 public class MainActivity extends AppCompatActivity {
 
